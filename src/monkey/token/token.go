bffe0744b3f6cf0ed8a4dbe98aaab5cf0479e41a
emahiro <hiromichi.a.ema@dena.com>
2018-07-18 01:52:37 +0900

    init

---
+++ src/monkey/token/token.go
@@ -0,0 +1,47 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y
+	INT   = "INT"   // 123456
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keyword
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// ユーザー定義のカスタムkeywords => 関数を指し示すときとかに使える
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
