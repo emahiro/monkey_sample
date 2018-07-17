7da971b729513b422a2f5ac8f3237717e8091e2f
emahiro <hiromichi.a.ema@dena.com>
2018-07-18 03:21:26 +0900

    fixed lexer

--- src/monkey/main.go
+++ src/monkey/main.go
@@ -1,7 +1,20 @@
 package main
 
-import `fmt`
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"monkey/repl"
+)
 
 func main() {
-	fmt.Printf("this is go imterpreter sample")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("hello %s!. this is the Monkey programming language. \n", user.Username)
+	fmt.Printf("feel free ato type in command following. \n")
+	repl.Start(os.Stdin, os.Stdout)
 }
